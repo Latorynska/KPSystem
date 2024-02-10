97622224ed20ecac9a5d91f383e65a3bf025d2a0
latorynska <paksiringkang225@gmail.com>
2024-02-11 06:05:23 +0700

    add : dashboard for mahasiswa

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
+
+use App\Models\KP;
+use App\Models\KPMetadata;
 
 class DashboardController extends Controller
 {
@@ -14,13 +18,30 @@ class DashboardController extends Controller
         if ($user->hasRole('mahasiswa')) {
             return $this->mahasiswaDashboard();
         } else {
+            dd('go to dash mahasiswa');
             return $this->defaultDashboard();
         }
     }
 
     protected function mahasiswaDashboard(){
-        return view('mahasiswa.dashboard');
+        $oneYearAgo = Carbon::now()->subYear();
+
+        $kps = KP::with('mahasiswa', 'metadata')
+            ->whereHas('metadata', function ($query) {
+                $query->where('status', 'done');
+            })
+            ->whereHas('metadata', function ($query) use ($oneYearAgo) {
+                $query->where('updated_at', '>=', $oneYearAgo);
+            })
+            ->get();
+        foreach($kps as $kp){
+            $kp->approvalDate = Carbon::parse($kp->metadata->updated_at)->translatedFormat('d F Y');
+        }
+
+        $data['kps'] = $kps;
+        return view('mahasiswa.dashboard', $data);
     }
+
     protected function defaultDashboard(){
         return view('dashboard');
     }
