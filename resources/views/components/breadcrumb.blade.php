58a55cdcede029e3edea3250b87fef2bfd73a2e0
latorynska <paksiringkang225@gmail.com>
2024-11-16 16:04:56 +0700

    update : dark and light mode support

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -13,12 +13,12 @@
     @endphp
 
         <li class="inline-flex items-center">
-            <a class="flex items-center text-sm text-gray-200 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500">
+            <a class="flex items-center text-sm text-gray-700 dark:text-gray-200 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500">
                 {{ $title }}
             </a>
             
             @if ($i < $count - 1)
-                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-700 dark:text-neutral-600 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="m9 18 6-6-6-6"/>
                 </svg>
             @endif
